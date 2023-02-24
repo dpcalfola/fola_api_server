b8e58fa92caac82bf3162fd1edfb3680a7d2ad96
Fola Flor <dpcalfola@gmail.com>
2023-02-25 01:31:51 +0900

    TDD > Test for "api_1" app
        * Test 1
            endpoint: /api_1/
            method: GET
            response: 200 OK
            response.data: {'message': 'Hello, REST API'}
    
        * Test 2
            endpoint: /api_1/<int:number>/
            method: GET
            response: 200 OK
            response.json:
            {
            'message': f'Get a number: {number}',
            'number': number
            }

--- Django_app/api_1/tests.py
+++ Django_app/api_1/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase # noqa
+from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class ResponseTests(TestCase):
+
+    def test_api_1_endpoint_returns_hello_rest_api_message(self):
+        """
+        Test that the /api_1/ endpoint returns expected message.
+        """
+        response = self.client.get('/api_1/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, {'message': 'Hello, REST API!'})
+
+    def test_api_1_get_integer_returns_expected_message_with_input_number(self):
+        """
+        Test that the /api_1/<int>/ endpoint returns expected message with input number.
+        """
+        number = 873
+        url = reverse('api-1', kwargs={'number': number})
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {
+            'message': f'Get a number: {number}',
+            'number': number
+        })
