8c2d3c8a7089308a713df5d4041733601075bcc9
Fola Flor <dpcalfola@gmail.com>
2023-02-25 00:27:07 +0900

    Feat > DRF setting, Startapp "api_1"
        * On Settings.py
            Separate INSTALLED_APPS
            into PACKAGE_APPS + THIRD_PARTY_APP + PROJECT_APP
    
        * Declare new app and package
            'api_1'
    
            'djangorestframework'
            'djangorestframework_authtoken'
            'drf_spectacular' (Swagger package)
    
        * Routing for Swagger
            'api/doc/'
            'api/schema/'
    
    	Note.
                'api/schema' is not working now
                because I've not write any serializer yet
    
        * Startapp "api_1"
            This app is for testing of simple Rest API code

--- Django_app/config/settings.py
+++ Django_app/config/settings.py
@@ -19,7 +19,6 @@ from .env.secret_env import ENV_SECRET_KEY
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -35,17 +34,30 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
-INSTALLED_APPS = [
+PACKAGE_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
 ]
+
+THIRD_PARTY_APP = [
+    'rest_framework',
+    'rest_framework.authtoken',
+    'drf_spectacular',
+]
+
+PROJECT_APP = [
+    'core_app',
+    'api_1',
+]
+
+INSTALLED_APPS = PACKAGE_APPS + THIRD_PARTY_APP + PROJECT_APP
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -77,7 +89,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
@@ -91,7 +102,6 @@ DATABASES = {
         'PASSWORD': os.environ.get('DB_PASS'),
     }
 }
-
 
 # DATABASES = {
 #     'default': {
@@ -119,7 +129,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
@@ -131,7 +140,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
@@ -141,3 +149,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# REST FRAMEWORK
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
